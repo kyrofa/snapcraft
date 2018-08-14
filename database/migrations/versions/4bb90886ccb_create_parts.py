0afcf104320ca1deb094d9e709bdc8e87ad770b2
Kyle Fazzari <kyrofa@ubuntu.com>
2018-08-14 07:56:01 -0700

    Progress

---
+++ database/migrations/versions/4bb90886ccb_create_parts.py
@@ -0,0 +1,27 @@
+"""create parts
+
+Revision ID: 4bb90886ccb
+Revises:
+Create Date: 2018-08-13 15:27:11.640567
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4bb90886ccb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'parts',
+        sa.Column('name', sa.String(), primary_key=True),
+    )
+
+
+def downgrade():
+    op.drop_table('parts')
